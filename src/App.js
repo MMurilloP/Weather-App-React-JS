4d49df57202ead9bac27d4d7ce4fccf0279346e7
Manuel Murillo <mmpeira@gmail.com>
2023-08-06 19:43:37 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import sunset from './assets/sunset3.mp4'
+
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=538659e0389fc7143f8b6e82543811d6&units=metric`;
+
+  const searchLocation = (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+      });
+      setLocation("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <video autoPlay loop muted className="video-background">
+        <source src={sunset} type="video/mp4" />
+      </video>
+      <div className="search">
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter Location"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}ºC</h1> : null}
+          </div>
+          <div className="icon">
+            {data.weather ? <img alt="weather-icon" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}/> : null}
+          </div>
+          <div className="description">
+            {data.weather ? (
+              <p>{data.weather[0].description}</p>
+            ) : null}
+          </div>
+        </div>
+
+        {data.name ? <div className="bottom">
+          <div className="feels">
+            {data.main ? (
+              <p className="bold">{data.main.feels_like.toFixed()}  ºC</p>
+            ) : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity}  %</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className="bold">{data.wind.speed.toFixed()}  km/h</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div> : null }
+        
+      </div>
     </div>
   );
 }
